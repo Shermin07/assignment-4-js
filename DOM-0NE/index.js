85b1ed35074fafc4b216c8149ad3cabf84f46769
Shermin Sultana <rima64549@gmail.com>
2023-08-12 03:52:47 +0600

    add

---
+++ DOM-0NE/index.js
@@ -0,0 +1,21 @@
+// getElementsByTag:::
+
+const fruitsContainer = document.getElementsByTagName("div");
+console.log(fruitsContainer);
+
+// getElementsById:::
+
+const mango = document.getElementById("mango");
+mango.innerHTML = "go to school";
+mango.style.color = "green";
+mango.style.fontSize = "1.5rem";
+
+// getElementsByClass::::
+
+const apple = document.getElementsByClassName("apple");
+apple.innerHTML = "jackfruit";
+
+
+// 
+const studentList = document.getElementsByClassName("studentA");
+studentList.innerHTML = "acdhhl";
